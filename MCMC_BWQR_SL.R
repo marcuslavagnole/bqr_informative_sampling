b0f33730796e0567dcf8a38735feafd02cab7f14
Marcus L. Nascimento <marcus.lavagnole@fgv.br>
2025-05-26 08:11:38 -0300

    Update MCMC_BWQR_SL.R

--- MCMC_BWQR_SL.R
+++ MCMC_BWQR_SL.R
@@ -15,10 +15,9 @@ condicionalBETA_MH <- function(beta,b,B,dados,x,w,tau){
 }
 
 # Metropolis-Hasting for the coefficients
-atualizarBETA_MH <- function(b,B,dados,x,beta,w,tau,ct,k,c0,c1){
+atualizarBETA_MH <- function(b,B,dados,x,beta,w,tau,ct,k){
   valoratual    <- beta
   sigma_MH      <- (tau*(1-tau))*chol2inv(chol((1/length(dados))*t(w^2*x)%*%x))
-  #valorproposto <- as.vector(rmvnorm(1, mean=valoratual, sigma=exp(log_ct)*sigma_MH))
   valorproposto <- as.vector(rmvnorm(1, mean=valoratual, sigma=ct*sigma_MH))
   candidato     <- exp(condicionalBETA_MH(valorproposto,b,B,dados,x,w,tau)-
                        condicionalBETA_MH(valoratual,b,B,dados,x,w,tau))
@@ -31,14 +30,14 @@ atualizarBETA_MH <- function(b,B,dados,x,beta,w,tau,ct,k,c0,c1){
     BETAfinal   <- valoratual
     aceita      <- 0
   }
-  log_ct        <- log(ct)+c0*(1/k^c1)*(chanceaceitar-0.234)
+  log_ct        <- log(ct)+(1/k^0.8)*(chanceaceitar-0.234)
   
   return(c(BETAfinal,aceita,exp(log_ct)))
 }
 
 
 # Bayesian Quantile Regression - Score based Likelihood
-bayesQRSL_weighted <- function(y,x,w,tau,n_mcmc,burnin_mcmc,thin_mcmc,cte,c0,c1){
+bayesQRSL_weighted <- function(y,x,w,tau,n_mcmc,burnin_mcmc,thin_mcmc){
   n         <- length(y)
   numcov    <- ncol(x)
   resultado <- list()
@@ -51,10 +50,10 @@ bayesQRSL_weighted <- function(y,x,w,tau,n_mcmc,burnin_mcmc,thin_mcmc,cte,c0,c1)
   contador   <- NULL
   const      <- NULL
   contador[1]<- 0
-  const[1]   <- cte
+  const[1]   <- 1
   # MCMC
   for(k in 2:n_mcmc){
-    beta_aux   <- atualizarBETA_MH(rep(0,numcov),diag(rep(1000,numcov)),y,x,beta[k-1,],w,tau,const[k-1],k,c0,c1)
+    beta_aux   <- atualizarBETA_MH(rep(0,numcov),diag(rep(1000,numcov)),y,x,beta[k-1,],w,tau,const[k-1],k)
     beta[k,]   <- beta_aux[1:numcov] ; contador[k] <- beta_aux[(numcov+1)] ; const[k] <- beta_aux[(numcov+2)] 
   }
   resultado[[1]]  <- beta[seq(burnin_mcmc+1,n_mcmc,thin_mcmc),]
